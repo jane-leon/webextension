996376b22666a2ad833da95ea39ceae7cbad5d78
Caroline Liu <cliu923@gmail.com>
2025-09-20 12:00:33 -0400

    popup.js changes

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
+/* Loads web extension. If user is on Netflix (the correct website), popup will prompt user to select a movie or TV show to 
+use the extension. If the user is not on Netflix, popup will prompt user to go to Netflix to use the extension :) */
 document.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentTab = tabs[0];
     if (currentTab && currentTab.url) {
       if (currentTab.url.includes('netflix.com')) {
         const content = document.querySelector('.content');
+        //TODO: TRY TO CHANGE SUCCESSMSG.STYLE TO GO IN POPUP.CSS AND TEST
         const successMsg = document.createElement('div');
         successMsg.style.cssText = `
             background: white;
@@ -18,10 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
         successMsg.innerHTML = '`<strong>Waiting to detect media</strong> Select a movie or TV show to get started!';
         content.appendChild(successMsg);
       } else {
-        // Add hint for non-Netflix pages
         const content = document.querySelector('.content');
-        const hint = document.createElement('div');
-        hint.style.cssText = `
+        //TODO: TRY TO CHANGE HINT.STYLE TO GO IN POPUP.CSS AND TEST
+        const hintMsg = document.createElement('div');
+        hintMsg.style.cssText = `
             background: white;
             color: #764ba2;
             border: 1px solid #d0b9ea;
@@ -31,8 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
             text-align: center;
             font-size: 13px;
           `;
-        hint.innerHTML = '<strong>Are you on Netflix?</strong> Go to Netflix.com to use this extension!';
-        content.appendChild(hint);
+        hintMsg.innerHTML = '<strong>Are you on Netflix?</strong> Go to Netflix.com to use this extension!';
+        content.appendChild(hintMsg);
       }
     }
   });
